6eeeed48fae721ecbfa249ff971862ce4b88ece3
Abhishek Rathore <rathore.abhishek87@gmail.com>
2017-02-16 11:59:15 +0530

    package json updated

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var ParseDashboard = require('parse-dashboard');
+
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
@@ -15,11 +17,9 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+ 
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -33,6 +33,23 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
+
+
+var dashboard = new ParseDashboard({
+  "apps": [
+    {
+      "serverURL": process.env.SERVER_URL || "http://localhost:1337/parse",
+      "appId": process.env.APP_ID || "myAppId",
+      "masterKey": process.env.MASTER_KEY || "myMasterKey",
+      "appName": "MyApp"
+    }],
+  "users" : [
+    {
+      user: "admin", // Used to log in to your Parse Dashboard
+      pass: "admin123"
+    }]
+},true);
+
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
